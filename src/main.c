d99204ba444cb8f9433c738b9eebec4cfcffcc76
Jacob Visovatti <jvisovatti@gmail.com>
2025-06-12 16:40:30 -0400

    Instantiate and print a small matrix.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World");
+    int matrix[2][2];
+
+    // Assign values like so:
+    // 1 2
+    // 3 4
+    int value = 1;
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            matrix[i][j] = value;
+            value++;
+        }
+    }
+
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            printf("%*d", 2, matrix[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }

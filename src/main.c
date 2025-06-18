86a86f2a4e8c7f1a48145545e9d4d4ca2301f323
Jacob Visovatti <jvisovatti@gmail.com>
2025-06-18 14:40:50 -0400

    Refactor matrix representation to use a struct.

--- src/main.c
+++ src/main.c
@@ -1,36 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_matrix(int** matrix, int rows, int cols);
+struct Matrix {
+    int rows;
+    int cols;
+    int** data;
+};
+
+void print_matrix(struct Matrix m);
 
 int main() {
 
-    int rows = 2;
-    int cols = 2;
-    int** m;
+    struct Matrix m = {
+        .rows = 2,
+        .cols = 2
+    };
 
-    m = malloc(sizeof *m * rows);
-    for (int i = 0; i < cols; i++) {
-        m[i] = malloc(sizeof *m[i] * cols);
+    m.data = malloc(sizeof *m.data * m.rows);
+    for (int i = 0; i < m.cols; i++) {
+        m.data[i] = malloc(sizeof *m.data[i] * m.cols);
     }
 
     int v = 5;
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            m[i][j] = v;
+    for (int i = 0; i < m.rows; i++) {
+        for (int j = 0; j < m.cols; j++) {
+            m.data[i][j] = v;
             v++;
         }
     }
 
-    print_matrix(m, rows, cols);
+    print_matrix(m);
 
     return 0;
 }
 
-void print_matrix(int** matrix, int rows, int cols) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%*d", 2, matrix[i][j]);
+void print_matrix(struct Matrix m) {
+    for (int i = 0; i < m.rows; i++) {
+        for (int j = 0; j < m.cols; j++) {
+            printf("%*d", 2, m.data[i][j]);
         }
         printf("\n");
     }

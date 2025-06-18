0a3c691d0cd0faefc3d426e431214e11b5ef6697
Jacob Visovatti <jvisovatti@gmail.com>
2025-06-18 14:05:57 -0400

    Represent matrix as 2D array and add helper function for printing.

--- src/main.c
+++ src/main.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void print_matrix(int** matrix, int rows, int cols);
 
 int main() {
-    int matrix[2][2];
 
-    // Assign values like so:
-    // 1 2
-    // 3 4
-    int value = 1;
-    for(int i = 0; i < 2; i++) {
-        for(int j = 0; j < 2; j++) {
-            matrix[i][j] = value;
-            value++;
+    int rows = 2;
+    int cols = 2;
+    int** m;
+
+    m = malloc(sizeof *m * rows);
+    for (int i = 0; i < cols; i++) {
+        m[i] = malloc(sizeof *m[i] * cols);
+    }
+
+    int v = 5;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            m[i][j] = v;
+            v++;
         }
     }
 
-    for(int i = 0; i < 2; i++) {
-        for(int j = 0; j < 2; j++) {
+    print_matrix(m, rows, cols);
+
+    return 0;
+}
+
+void print_matrix(int** matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             printf("%*d", 2, matrix[i][j]);
         }
         printf("\n");
     }
-
-    return 0;
 }

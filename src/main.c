f3f0a12f57e5f06d95b35ab165bb33ddd01d107f
Jacob Visovatti <jvisovatti@gmail.com>
2025-06-20 15:31:24 -0400

    Revise initialization helper to support optional array of values to populate a matrix.

--- src/main.c
+++ src/main.c
@@ -10,39 +10,75 @@ struct Matrix {
     int** data;
 };
 
+typedef enum InitStrategy InitStrategy;
+
+enum InitStrategy {
+    RANDOM,
+    FROM_ARRAY
+};
+
 void print_matrix(Matrix m);
 
 void matmul(Matrix* a, Matrix* b, Matrix* result);
 
-void init_matrix(Matrix* m);
+int init_matrix(Matrix* m, int rows, int cols, InitStrategy strategy, int* values);
 
 int main() {
 
     srand((unsigned int)time(NULL));
 
-    Matrix a = {
-        .rows = 2,
-        .cols = 2
-    };
+    Matrix a;
+    if (init_matrix(&a, 2, 2, RANDOM, NULL) != 0) {
+        printf("Failed to initialize matrix.\n");
+        return 1;
+    }
 
-    init_matrix(&a);
+    Matrix b;
+    int values[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    if (init_matrix(&b, 3, 3, FROM_ARRAY, values) != 0) {
+        printf("Failed to initialize matrix.\n");
+        return 1;
+    }
 
     print_matrix(a);
+    print_matrix(b);
 
     return 0;
 }
 
-void init_matrix(Matrix* m) {
+int init_matrix(Matrix* m, int rows, int cols, InitStrategy strategy, int* values) {
+    m->rows = rows;
+    m->cols = cols;
     m->data = malloc(sizeof *m->data * m->rows);
     for (int i = 0; i < m->cols; i++) {
         m->data[i] = malloc(sizeof *m->data[i] * m->cols);
     }
 
-    for (int i = 0; i < m->rows; i++) {
-        for (int j = 0; j < m->cols; j++) {
-            m->data[i][j] = rand() % 100;
-        }
+    switch (strategy) {
+        case RANDOM:
+            for (int i = 0; i < m->rows; i++) {
+                for (int j = 0; j < m->cols; j++) {
+                    m->data[i][j] = rand() % 100;
+                }
+            }
+            break;
+        case FROM_ARRAY:
+            if (!values) {
+                printf("Array of values for matrix initialization must not be null.\n");
+                return 1;
+            }
+            /* TODO: We also assume here that the `values` array has at least `rows * cols` elements. */
+            int index = 0;
+            for (int i = 0; i < m->rows; i++) {
+                for (int j = 0; j < m->cols; j++) {
+                    m->data[i][j] = values[index];
+                    index++;
+                }
+            }
+            break;
     }
+
+    return 0;
 }
 
 void matmul(Matrix* a, Matrix* b, Matrix* result) {
